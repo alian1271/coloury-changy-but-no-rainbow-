5d0670c2e89a0627a58d377c07e1aca5f0850035
alian1271 <89016788+alian1271@users.noreply.github.com>
2021-09-24 16:37:45 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,31 @@
+
+function setup() {
+  createCanvas(400,400);
+}
+
+
+function draw() 
+{
+  background(30);
+  if (keyIsDown(UP_ARROW)){
+background('red')
+
+  }
+  if (keyIsDown(DOWN_ARROW)){
+    background("blue")
+    
+      }
+
+      if (keyIsDown(RIGHT_ARROW)){
+        background("green")
+        
+          }
+
+          if (keyIsDown(LEFT_ARROW)){
+            background("cyan")
+            
+              }
+}
+
+
+
